3302ede0ed2cc543cfc2056f96bf7cae8f7a531c
Lucas Popp <lmp122@case.edu>
2024-01-19 17:15:39 -0800

    config-ify home assistant ip

--- app/sockets.py
+++ app/sockets.py
@@ -1,7 +1,7 @@
 import socket
 from . import config
 
-homeAddr = socket.getaddrinfo('192.168.86.44', 8123)[0][-1]
+homeAddr = socket.getaddrinfo(config.value["home-assistant-ip"], 8123)[0][-1]
 
 def new_socket():
     s = socket.socket()
