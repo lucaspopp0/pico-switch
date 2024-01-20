7496766528bcb40bb44f36437eaf7deb79ba1332
Lucas Popp <lmp122@case.edu>
2024-01-19 17:19:23 -0800

    add ota_updater

--- app/start.py
+++ app/start.py
@@ -1,11 +1,38 @@
-import time
-from . import wifi
-from . import board
-from . import config
+def connectToWifiAndUpdate():
+    import time, machine, network, gc, app.secrets as secrets
+    time.sleep(1)
+    print('Memory free', gc.mem_free())
 
-board.led.off()
-config.read()
-wifi.connect()
+    from app.ota_updater import OTAUpdater
 
-while True:
-    board.request_queue.poll()
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+    otaUpdater = OTAUpdater('https://github.com/rdehuyss/micropython-ota-updater', main_dir='app', secrets_file="secrets.py")
+    hasUpdated = otaUpdater.install_update_if_available()
+    if hasUpdated:
+        machine.reset()
+    else:
+        del(otaUpdater)
+        gc.collect()
+
+def startApp():
+    from . import wifi
+    from . import board
+    from . import config
+
+    board.led.off()
+    config.read()
+    wifi.connect()
+
+    while True:
+        board.request_queue.poll()
+
+
+connectToWifiAndUpdate()
+startApp()
