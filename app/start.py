eecb4860a52523a2095ebb2ca0efa03fc96147ad
Lucas Popp <lmp122@case.edu>
2024-01-20 18:23:28 -0800

    move more into start/try loop

--- app/start.py
+++ app/start.py
@@ -4,26 +4,25 @@ def connectToWifiAndUpdate():
     print('Memory free', gc.mem_free())
 
     try:
+        from app.ota_updater.ota_updater import OTAUpdater
         import app.config as config, app.board as board, app.wifi as wifi
         config.read()
         board.setup()
+
+        wifi.connect()
+        otaUpdater = OTAUpdater('https://github.com/lucaspopp0/pico-switch', main_dir='app')
+
+        if otaUpdater.check_for_update_to_install_during_next_reboot():
+            board.led.do_color(50, 10, 0)
+
+        hasUpdated = otaUpdater.install_update_if_available()
+        if hasUpdated:
+            machine.reset()
+        else:
+            del(otaUpdater)
+            gc.collect()
     except Exception as e:
         print(e)
-
-    from app.ota_updater.ota_updater import OTAUpdater
-
-    wifi.connect()
-    otaUpdater = OTAUpdater('https://github.com/lucaspopp0/pico-switch', main_dir='app')
-
-    if otaUpdater.check_for_update_to_install_during_next_reboot():
-        board.led.do_color(50, 10, 0)
-
-    hasUpdated = otaUpdater.install_update_if_available()
-    if hasUpdated:
-        machine.reset()
-    else:
-        del(otaUpdater)
-        gc.collect()
 
 def startApp():
     from . import wifi
