e0a013b02165c11b713a3f7cf1cb517bd786c911
lucaspopp0 <lucaspopp0@gmail.com>
2025-07-14 22:09:57 -0700

    fix

--- app/shared.py
+++ app/shared.py
@@ -1,9 +1,9 @@
-from .requestqueue import request
+from .requestqueue import request, queue
 from .board import board
 from .config import config
 from .wifi import wifi
 
-shared_rq: request.RequestQueue
+shared_rq: queue.RequestQueue
 shared_board: board.Board
 shared_cfg: config.Config
 shared_wifi: wifi.WiFiController
